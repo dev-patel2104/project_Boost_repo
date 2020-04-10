0e9eea953acdf30d533bfb834833bdc16cc3e453
Dev Patel <59513155+dev-patel2104@users.noreply.github.com>
2020-04-10 21:29:48 +0530

    game application beta

---
+++ scripts/oscillator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector = new Vector3 (10f,10f,10f);
+    [Range(0, 1)] [SerializeField] float movementFactor;
+    [SerializeField] float period = 2f;
+    [SerializeField] Vector3 offset = new Vector3(0f,0f,0f);
+
+    Vector3 startPos;
+    
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (period <= Mathf.Epsilon)
+        {
+            return;
+        }
+
+        else
+        {
+            float cycles = Time.time / period;
+            const float tau = Mathf.PI * 2;
+            float rawSin = Mathf.Sin(cycles * tau);
+            movementFactor = rawSin / 2 + 0.5f;
+            offset = movementVector * movementFactor;
+            transform.position = startPos + offset;
+        }
+    }
+}
---
+++ scripts/rocketMovement.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class rocketMovement : MonoBehaviour
+{
+    [SerializeField] float rocketSpeed = 5f;
+    [SerializeField] float rocketRotate = 5f;
+    [SerializeField] AudioClip ThrustSound;
+    [SerializeField] AudioClip DeathSound;
+    [SerializeField] AudioClip SuccessSound;
+    [SerializeField] float NextSceneLoadTime = 1.5f;
+    [SerializeField] ParticleSystem ThrustVFX;
+    [SerializeField] GameObject ExplosionVFX;
+    
+
+    Rigidbody rocketRG;
+    AudioSource rocketSource;
+    int currentIndex;
+    int temp;
+    int SuccessCnt;
+    
+    void Start()
+    {
+        ThrustVFX.enableEmission = false;
+        rocketRG = GetComponent<Rigidbody>();
+        rocketSource = GetComponent<AudioSource>();
+        currentIndex = SceneManager.GetActiveScene().buildIndex;
+        temp = 0;
+        SuccessCnt = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (temp == 0)
+        {
+
+            rocketRotation();
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    rocketSource.PlayOneShot(ThrustSound);
+                    ThrustVFX.enableEmission = true;
+                    ThrustVFX.Play();
+                }
+                Debug.Log("space");
+                rocketThrust();
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                rocketSource.Stop();
+                ThrustVFX.Stop();
+            }
+
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    public void rocketRotation()
+    {
+        
+        if(Input.GetKey(KeyCode.A))
+        {
+            var rotateAmount = rocketRotate;
+            Vector3 rotateVector = transform.forward * rotateAmount;
+            rocketRG.MoveRotation(Quaternion.Euler(rocketRG.rotation.eulerAngles + rotateVector * rocketRotate));
+        }
+
+        else if(Input.GetKey(KeyCode.D))
+        {
+            var rotateAmount = -rocketRotate;
+            Vector3 rotateVector = transform.forward * rotateAmount;
+            rocketRG.MoveRotation(Quaternion.Euler(rocketRG.rotation.eulerAngles + rotateVector * rocketRotate));
+        }
+        
+
+    }
+
+    public void rocketThrust()
+    {
+        rocketRG.AddRelativeForce(rocketSpeed * Time.fixedDeltaTime * Vector3.up* rocketSpeed);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if(temp != 0)
+        {
+            return;
+        }
+        switch(collision.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("OKKK");
+                break;
+            case "obstacle":
+                ProcessDeath();
+                break;
+            case "Finish":
+                ProcessSuccess();
+                break;
+            default:
+                Debug.Log("Untagged");
+                break;
+        }
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentIndex + 1);
+        
+    }
+
+    private void LoadFirstScene()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    private void ProcessDeath()
+    {
+        GameObject Death;
+        rocketRG.freezeRotation = false;
+        temp++;
+        rocketSource.Stop();
+        rocketSource.PlayOneShot(DeathSound);
+        Invoke("LoadFirstScene", NextSceneLoadTime);
+        Death = Instantiate(ExplosionVFX, transform.position, Quaternion.identity);
+        
+    }
+
+    private void ProcessSuccess()
+    {
+        if(SuccessCnt == 0)
+        {
+            rocketSource.Stop();
+            rocketSource.PlayOneShot(SuccessSound);
+            Invoke("LoadNextScene", NextSceneLoadTime);
+        }
+        else
+        {
+            return;
+        }
+        SuccessCnt++;
+
+    }
+}
